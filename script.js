43ed73b47e501798b726efe305659752018f2c10
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-05-29 02:04:15 -0700

    finished styling iphone calculator

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class Calculator {
         this.symbols = ["AC", "+/-", "%", "÷",
                         7, 8, 9, "x", 4, 5, 6,
                         "-", 1, 2, 3, "+", 0,
-                        ".", "="
+                        "", ".", "="
                     ];
     }
 
@@ -28,6 +28,12 @@ class Calculator {
 
         for (let i = 0; i < this.symbols.length; i++) {
             const circle = document.createElement("btn");
+            circle.setAttribute("id", `${i}`);
+            if (i < 3) {
+                circle.style.backgroundColor = "#D4D4D2";
+            } else if ((i > 3 && i < 7) || (i > 7 && i < 11) || (i > 11 && i < 15) || (i > 15 && i < 18)) {
+                circle.style.backgroundColor = "#505050";
+            }
             circle.innerHTML = this.symbols[i];
             circle.classList.add("circle");
             calcBody.append(circle);
