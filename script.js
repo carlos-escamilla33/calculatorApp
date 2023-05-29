21e0991537f64c47cbcebad2238c51a18102959c
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-05-28 22:24:14 -0700

    created calculator class

---
+++ script.js
@@ -0,0 +1,25 @@
+class Calculator {
+    constructor() {
+        this.color = color;
+    }
+
+    calcAdd(num1, num2) {
+        return num1 + num2;
+    }
+
+    calcSub(num1, num2) {
+        return num1 - num2;
+    }
+
+    calcDiv(num1, num2) {
+        return num1 / num2;
+    }
+
+    calcMult(num1, num2) {
+        return num1 * num2;
+    }
+
+    createButtons() {
+        const calcBody = document.querySelector("#calculator");
+    }
+}
