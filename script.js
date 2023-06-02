819fdc27cf3ecf26fa7e28253f7d2d4c01f61ff3
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-06-02 09:27:35 -0700

    changed code from OOP to functional

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-class Calculator {
-    constructor() {
-        this.symbols = ["AC", "+/-", "%", "÷",
-                        7, 8, 9, "x", 4, 5, 6,
-                        "-", 1, 2, 3, "+", 0,
-                        "", ".", "="
-                    ];
-    }
+const symbols = ["AC", "+/-", "%", "÷",
+    7, 8, 9, "x", 4, 5, 6,
+    "-", 1, 2, 3, "+", 0,
+    "", ".", "="
+];
 
-    calcAdd(num1, num2) {
-        return num1 + num2;
-    }
 
-    calcSub(num1, num2) {
-        return num1 - num2;
-    }
+const createButtons = () => {
+    const calcBody = document.querySelector(".keys");
 
-    calcDiv(num1, num2) {
-        return num1 / num2;
-    }
-
-    calcMult(num1, num2) {
-        return num1 * num2;
-    }
-
-    createButtons() {
-        const calcBody = document.querySelector(".keys");
-
-        for (let i = 0; i < this.symbols.length; i++) {
-            const circle = document.createElement("btn");
-            circle.setAttribute("id", `${i}`);
-            if (i < 3) {
-                circle.style.backgroundColor = "#D4D4D2";
-            } else if ((i > 3 && i < 7) || (i > 7 && i < 11) || (i > 11 && i < 15) || (i > 15 && i < 18)) {
-                circle.style.backgroundColor = "#505050";
-            }
-            circle.innerHTML = this.symbols[i];
-            circle.classList.add("circle");
-            calcBody.append(circle);
+    for (let i = 0; i < this.symbols.length; i++) {
+        const circle = document.createElement("btn");
+        circle.setAttribute("id", `${i}`);
+        if (i < 3) {
+            circle.style.backgroundColor = "#D4D4D2";
+        } else if ((i > 3 && i < 7) || (i > 7 && i < 11) || (i > 11 && i < 15) || (i > 15 && i < 18)) {
+            circle.style.backgroundColor = "#505050";
         }
+        circle.innerHTML = this.symbols[i];
+        circle.classList.add("circle");
+        calcBody.append(circle);
     }
 }
 
-const main  = () => {
-    let iphoneCalc = new Calculator();
-    iphoneCalc.createButtons();
-}
 
-main();
+const main = () => {
+
+
+}
